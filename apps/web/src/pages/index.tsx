909ad88b08d41ef447d21920092d69895da77fed
Radialarray <25952866+Radialarray@users.noreply.github.com>
2022-08-29 11:16:07 +0200

    refactor(web): extract all components in extra files, refactor imports

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 import Layout from '../lib/components/Layout';
-import styles from '../styles/Home.module.css';
+import styles from '../lib/styles/Home.module.css';
 
 const Home: NextPage = () => {
 	return (
