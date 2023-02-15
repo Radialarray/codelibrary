4b164cfd4073ea435dc905711b2bc10ee2797b2b
Sven <sven.loechner@gmx.de>
2023-02-15 15:27:59 +0100

    feat(cms): kirby-navigation plugin update and add to composer.json

--- apps/cms/site/plugins/kirby-navigation/src/index.js
+++ apps/cms/site/plugins/kirby-navigation/src/index.js
@@ -1,9 +1,9 @@
-import VueNestable from 'vue-nestable/dist/index.esm';
-import Field from './Field.vue';
+import VueNestable from "vue-nestable/dist/index.esm";
+import Field from './Field.vue'
 
 panel.plugin('beluga/navigation', {
-	fields: {
-		navigation: Field
-	},
-	use: VueNestable
-});
+  fields: {
+    navigation: Field
+  },
+  use: VueNestable
+})
