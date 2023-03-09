4d4fb55a3a903a7af8ded6aa04074e17056f36be
Sven <sven.loechner@gmx.de>
2023-03-09 10:06:18 +0100

    feat(cms): fieldmethod for layout image url generation
    Finally works!!

--- apps/cms/site/plugins/kql-image-paths/index.php
+++ apps/cms/site/plugins/kql-image-paths/index.php
@@ -4,92 +4,26 @@ use Kirby\Cms\App as Kirby;
 
 Kirby::plugin('radialarray/addImagePaths', [
   'fieldMethods' => [
-    'addImagePaths' => function ($field) {
-      // function iterateLayouts($layout)
-      // {
-      //   return array_map("iterateColumns", $layout->columns());
-      // }
-
-      // function iterateColumns($columns)
-      // {
-      //   return $columns;
-      //   return array_map("iterateBlocks", $columns);
-      // }
-
-      // function iterateBlocks($blocks)
-      // {
-      //   return gettype($blocks);
-      //   return array_map("iterateBlock", $blocks);
-      // }
-
-      // function iterateBlock($block)
-      // {
-      //   return $block;
-      // }
-
-
-      // $layouts = $field->toLayouts()->toArray();
-
-      // $newLayouts = array_map("iterateLayouts", $layouts);
-
-      // return $newLayouts;
-
-
-      // For blocks!
-      // $model = $field->parent();
-      // $blocks = $field->toBlocks()->toArray();
-
-      // For layouts!
-      // $model = $field->parent();
-      $layouts = $field->toLayouts();
-      $blocks  = $layouts->toBlocks()->toArray();
-
-
-      $model = $layouts->toBlocks()->parent();
+    'addImagePathsToLayout' => function ($field) {
+      $model = $field->parent();
+      $layouts = $field->toLayouts()->toArray();
 
       if (class_uses($model, 'Kirby\Cms\HasFiles')) {
-        // return $model;
-        array_walk_recursive($blocks, function (&$value, $key) use ($model) {
+        array_walk_recursive($layouts, function (&$value, $key) use ($model) {
           if (is_int($key) && is_string($value)) {
             if ($file = $model->file($value)) {
+              // $value = $file->srcset([300, 800, 1024]);
               $value = $file->url();
-              // $value = $key;
             }
           }
-          // return $model;
         });
       }
 
-      $fieldLayouts = json_decode($field->toString(), true);
-
-      foreach ($fieldLayouts as $layoutKey => $layout) {
-        $columns = $layout['columns'];
-        foreach ($columns as $columnKey => $column) {
-          $block = $column['blocks'];
-          if (isset($block[0]['content']['image'])) {
-            foreach ($blocks as $blockSearch) {
-              if ($blockSearch['id'] === $block[0]['id']) {
-                if (isset($blockSearch['content']['image'][0])) {
-                  $image = $blockSearch['content']['image'][0];
-                  $fieldLayouts[$layoutKey]['columns'][$columnKey]['blocks'][0]['content']['image'][0] = $image;
-                }
-              }
-            }
-          } else if (isset($block[0]['content']['images'])) {
-            foreach ($blocks as $blockSearch) {
-              if ($blockSearch['id'] === $block[0]['id']) {
-                if (isset($blockSearch['content']['images'])) {
-                  foreach ($blockSearch['content']['images'] as $key => $image) {
-                    $fieldLayouts[$layoutKey]['columns'][$columnKey]['blocks'][0]['content']['images'][$key] = $image;
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-
-      return $fieldLayouts;
+      return $layouts;
+    },
+    'addImagePath' => function ($field) {
+      $file = $field->toFile();
+      return $file;
     }
   ],
 ]);
